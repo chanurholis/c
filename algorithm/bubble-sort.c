660c27e23e8eae7935425e9b127c061004fdac96
Chacha Nurholis <chachanurholis29@gmail.com>
2023-08-05 21:22:43 +0700

    fix

--- algorithm/bubble-sort.c
+++ algorithm/bubble-sort.c
@@ -23,10 +23,7 @@ int main()
   int arr[] = {64, 34, 25, 12, 22, 11, 90};
 
   int n = sizeof(arr) / sizeof(arr[0]);
-
-  printf("%d", n);
-  return 0;
-
+  
   printf("Original array: ");
   for (int i = 0; i < n; i++)
   {
